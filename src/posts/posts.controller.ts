c31f23e313419498bafa26750f65baf3d52d1765
Hemant Negi <nhemnt@gmail.com>
2021-07-28 13:46:41 +0530

    post contoller routes

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    return `Get all posts`;
+  }
+
+  @Get('/:postId')
+  getPostDetails(@Param() param): string {
+    return `detail of post = ${param.postId}`
+  }
+
+  @Post('/')
+  createNewPost(): string {
+    return `new post was created`;
+  }
+
+  @Delete('/:postId')
+  deletePost(@Param() param): string {
+    return `delete post id = ${param.postId}`;
+  }
+
+  @Put('/:postId/like')
+  likePost(@Param() param): string {
+    return `liked post ${param.postId}`;
+  }
+
+  @Delete('/:postId/like')
+  unlikePost(@Param() param): string {
+    return `unliked post ${param.postid}`;
+  }
+}
