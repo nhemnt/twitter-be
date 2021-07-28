b8516caeb7320ef52699a70c531b4409d0c4c3c7
Hemant Negi <nhemnt@gmail.com>
2021-07-28 13:40:26 +0530

    endpoints for user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/@:username')
+  getUserByUsername(@Param() param): string {
+    //Todo: add actual logic
+    return `detail of username = ${param.username}`;
+  }
+
+  @Get('/:userid')
+  getUserByUserid(@Param() param): string {
+    //Todo: add actual logic
+    return `detail of user id = ${param.userid}`;
+  }
+
+  @Post('/')
+  createNewUser(): string {
+    return 'new user created';
+  }
+
+  @Patch('/:userId')
+  updateUserDetails(@Param() param): string {
+    return `details of user ${param.userid} is updated`;
+  }
+
+  @Put('/:userId/follow')
+  followUser(): string {
+    return 'you followed user';
+  }
+
+  @Delete('/:userId/follow')
+  unfollowUser(): string {
+    return 'you unfollowed user';
+  }
+
+  @Get('/:userId/followers')
+  getFollowersOfUser(): string {
+    return `get followers if user`;
+  }
+
+  @Put('/:userId/followees')
+  getFolloweesOfUser(): string {
+    return `get followees of given user`;
+  }
+}
